47eba35805ce6ba6010e200f7455a9bf9f2ccde2
amityadav0612 <amity7205@gmail.com>
2025-07-24 19:21:26 +0530

    initalize the project

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app=express();
+
+const apiRoutes=require('./routes');
+
+
+
+
+app.use('/api',apiRoutes)
+
+
+const { ServerConfig ,Logger} = require('./config');
+
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Server is running on port :${ServerConfig.PORT}`);
+    Logger.info("successfully started server","root",{});
+})
